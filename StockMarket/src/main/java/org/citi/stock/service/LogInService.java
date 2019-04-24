f8944b1c42d69619a3b89f3ad77c54b93f3ab93e
msamruddhi <31569718+msamruddhi@users.noreply.github.com>
2019-04-24 13:10:16 +0530

    Add files via upload

---
+++ StockMarket/src/main/java/org/citi/stock/service/LogInService.java
@@ -0,0 +1,42 @@
+package org.citi.stock.service;
+
+import org.citi.stock.bean.LogIn;
+import org.citi.stock.db.DBConnection;
+
+import java.sql.*;
+
+public class LogInService {
+	
+	
+	public LogInService() {
+		super();
+		
+	}
+	
+	public int checkValidUser(LogIn user) throws Exception
+	{
+		
+		Connection con=DBConnection.getConnectionfunc();
+		Statement stmt=con.createStatement();
+		
+		String query="select password from login where username=? AND password=?";
+				
+		 PreparedStatement getPass=con.prepareStatement(query);
+		 getPass.setString(1, user.getUsername());
+		 getPass.setString(2, user.getPassword());
+		 ResultSet rs=getPass.executeQuery();
+		
+		 if(rs.isBeforeFirst())
+		 {
+			 rs.next();
+			// System.out.println("Not empty");
+			 return 1;
+		 }
+		 else {
+			// System.out.println(" empty");
+			 return 0;
+		 }
+		
+		
+	}
+}
